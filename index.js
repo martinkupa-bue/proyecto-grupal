c73fbd63bb8126cb73e630bf253e6ba04cf35d57
martin kupa <kupa@mail.com>
2024-08-06 08:45:29 -0300

    agregue una ruta nueva

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World! This is my first webpage')
 })
 
+app.get("/login", (req, res) => res.send("Aqui estara el login"))
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
